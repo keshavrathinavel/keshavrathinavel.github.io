2a7c6c35453a51d3f0b9a7773f5fbc47ea0741c8
Keshav Rathinavel <keshavrathinavel@gmail.com>
2024-06-30 01:20:59 +0530

    Add typing animation to result in Terminal

--- src/app/components/Terminal.tsx
+++ src/app/components/Terminal.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import handleCommand from "@/lib/handleCommand";
+import { TypeAnimation } from "react-type-animation";
 
 interface CommandOutput {
     command: string
@@ -11,6 +12,8 @@ interface CommandOutput {
 export default function Terminal() {
     const [input, setInput] = useState<string>('')
     const [output, setOutput] = useState<CommandOutput[]>([])
+    const [currentLineIndex, setCurrentLineIndex] = useState<number>(0)
+    const [currentOutputIndex, setCurrentOutputIndex] = useState<number>(0)
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -25,6 +28,46 @@ export default function Terminal() {
         setInput('')
     }
 
+    useEffect(() => {
+        if (currentOutputIndex < output.length) {
+            const result = output[currentOutputIndex].result;
+            if (Array.isArray(result) && currentLineIndex < result.length) {
+                const timer = setTimeout(() => {
+                    setCurrentLineIndex(currentLineIndex + 1)
+                }, 75);
+
+                return () => clearTimeout(timer)
+            } else if (!Array.isArray(result) || currentLineIndex >= result.length) {
+                const timer = setTimeout(() => {
+                    setCurrentOutputIndex(currentOutputIndex + 1)
+                    setCurrentLineIndex(0)
+                }, 0);
+
+                return () => clearTimeout(timer)
+            }
+        }
+    }, [currentLineIndex, currentOutputIndex, output])
+
+    const renderResult = (result: string | string[], outputIndex: number) => {
+        if (Array.isArray(result)) {
+            return (
+                <div>
+                    {result.slice(0, outputIndex === currentOutputIndex ? currentLineIndex : result.length).map((line, idx) => (
+                        <div key={idx}>
+                            <TypeAnimation sequence={[line]} speed={99} cursor={false}/>
+                        </div>
+                    ))}
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <TypeAnimation sequence={[result]} speed={99} cursor={false}/>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className='ml-4'>
             <div>
@@ -34,11 +77,7 @@ export default function Terminal() {
                             className='text-green-300'>~</span> learn@about.keshav$ <span
                             className='text-white font-normal'>{output.command}</span></span>
                         <div className='px-4 py-2'>
-                            <pre>{Array.isArray(output.result) ? (
-                                output.result.map((line, idx) => <div key={idx}>{line}</div>)
-                            ) : (
-                                <div>{output.result}</div>
-                            )}</pre>
+                            <pre>{renderResult(output.result, index)}</pre>
                         </div>
                     </div>
                 ))}
@@ -46,7 +85,7 @@ export default function Terminal() {
             <form onSubmit={handleSubmit}>
                 <span className='text-pink-300 font-semibold'><span className='text-green-300'>~</span> learn@about.keshav$ </span>
                 <input type='text'
-                       className='bg-transparent focus:outline-none text-white'
+                       className='bg-transparent focus:outline-none text-white caret-green-300'
                        value={input}
                        onChange={(e) => setInput(e.target.value)}
                        autoFocus/>
