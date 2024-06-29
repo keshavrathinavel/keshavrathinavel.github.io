c998c2378d49aaf69b28411c903e88b975b5ab69
Keshav Rathinavel <keshavrathinavel@gmail.com>
2024-06-29 18:57:53 +0530

    Add padding to output so it looks separated from command input

--- src/app/components/Terminal.tsx
+++ src/app/components/Terminal.tsx
@@ -26,14 +26,14 @@ export default function Terminal() {
     }
 
     return (
-        <div className='ml-2'>
+        <div className='ml-4'>
             <div>
                 {output.map((output, index) => (
                     <div key={index}>
                         <span className='text-pink-300 font-semibold'><span
                             className='text-green-300'>~</span> learn@about.keshav$ <span
                             className='text-white font-normal'>{output.command}</span></span>
-                        <div>
+                        <div className='px-4 py-2'>
                             {Array.isArray(output.result) ? (
                                 output.result.map((line, idx) => <div key={idx}>{line}</div>)
                             ) : (
