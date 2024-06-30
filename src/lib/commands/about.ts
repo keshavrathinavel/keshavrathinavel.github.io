1712270f5f5dde14641a105d10aec2072b1a38d1
Keshav Rathinavel <keshavrathinavel@gmail.com>
2024-07-01 01:25:44 +0530

    Add content for 'about' command

--- src/lib/commands/about.ts
+++ src/lib/commands/about.ts
@@ -1,5 +1,8 @@
 export default function about(): string[] {
     return [
-        'about'
+        'I am a full-stack developer from India. I love working on solutions to ambiguous problems.',
+        'The uncertainty of the domain is what excites me. When the solution adds value to the user, it\'s a win-win.',
+        'Every line of code serves both a product vision and an entrepreneurial spirit.',
+        'I also love to draw and cook. And I ride bikes fast 🏍️'
     ]
 }
