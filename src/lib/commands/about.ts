1ba470469e74793e4215beac8fabe8b24bc74c49
Keshav Rathinavel <keshavrathinavel@gmail.com>
2024-07-07 22:57:22 +0530

    Rephrase sentence in about command

--- src/lib/commands/about.ts
+++ src/lib/commands/about.ts
@@ -1,7 +1,7 @@
 export default function about(): string[] {
     return [
         'I am a full-stack developer from India. I love working on solutions to ambiguous problems.',
-        'The uncertainty of the domain is what excites me. When the solution adds value to the user, it\'s a win-win.',
+        'The uncertainty of the domain is what excites me. When the solution adds value to someone, it\'s a win-win.',
         'Every line of code serves both a product vision and an entrepreneurial spirit.',
         'I also love to draw and cook. And I ride bikes fast 🏍️'
     ]
