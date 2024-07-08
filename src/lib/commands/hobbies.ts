8c7932b68308e28f3366ec8fc7de9e8745d5f27f
Keshav Rathinavel <keshavrathinavel@gmail.com>
2024-07-08 23:48:27 +0530

    Remove reference to 'funfact' from 'hobbies' command

--- src/lib/commands/hobbies.ts
+++ src/lib/commands/hobbies.ts
@@ -2,9 +2,8 @@ export default function hobbies(): string[] {
     return [
         'I can cook a mean pasta 🍝',
         'I lift weights 🏋️‍♂️ love pushing my limits and seeing the results.',
-        'I like drawing ✏️ I wonder, if I stuck with it, would I have taken it up professionally?',
+        'I like drawing.️ I wonder, if I stuck with it, would I still be making cool shit like this?',
         'I like reading books when I get the time to. Open to recommendations 📚',
         'I love riding bikes 🏍️ I can\'t wait to zip through the mountains, the plan is made, just waiting to buy the bike.',
-        'I follow Formula 1 🏎️ I can make accurate car sounds too. Want to know more? Type \'funfact\'',
     ]
 }
