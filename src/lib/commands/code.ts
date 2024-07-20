7878fa83ee36fee68441f35e3ab20233a6695a3d
keshavrathinavel <keshavrathinavel@gmail.com>
2024-07-20 12:47:57 +0530

    Update URL in code.ts to reflect repository name change

--- src/lib/commands/code.ts
+++ src/lib/commands/code.ts
@@ -2,7 +2,7 @@ export default function code(): { text: string, url: string }[] {
     return [
         {
             text: 'Here you go :)',
-            url: 'https://github.com/keshavrathinavel/portfolio',
+            url: 'https://github.com/keshavrathinavel/keshavrathinavel.github.io/',
         }
     ]
 }
